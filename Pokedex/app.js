5dd9a7acc035e08a9fd80d7bf4a369960ab22f51
“Tony <tcabezon@gmail.com>
2020-08-27 23:54:09 -0400

    added for let, pics of wireframe, draw.io wireframe draft

--- Pokedex/app.js
+++ Pokedex/app.js
@@ -1,7 +1,34 @@
 console.log("Compassion = Happiness");
 
-function getPokemonInfo() {
-  ajax_get("https://pokeapi.co/api/v2/", function (data) {
-    populatePokemonSelect(data);
-  });
+const pokemonContainer = document.getElementById(`pokemonContainer`);
+const pokemonAmount = 151;
+
+// function to call pokemon
+const fetchPokemon = async () => {
+  for (let i = 1; i <= pokemonAmount; i++) {
+    await getPokemon(i);
+  }
+};
+
+const getPokemon = async (id) => {
+  const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+  const res = await fetch(url);
+  const pokemon = await res.json();
+  makePokemonCard(pokemon);
+};
+
+// shows return data
+fetchPokemon();
+
+function makePokemonCard(pokemon) {
+  const pokemonElement = document.createElement(`div`);
+  pokemonElement.classList.add(`pokemon`);
+
+  const name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1);
+
+  const pokemonInnerHTML = `
+  <div class="image-container"></div>
+  
+    pokemonElement.innerHTML = pokemonInnerHTML;
+  pokemonContainer.appendChild(pokemonElement);
 }
