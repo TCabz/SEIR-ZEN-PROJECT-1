04169a9233845929a8ae8db79f6593781e799dc2
“Tony <tcabezon@gmail.com>
2020-08-29 05:13:42 -0400

    push correct project files, not completed yet

--- Pokedex/app.js
+++ Pokedex/app.js
@@ -1,34 +1,37 @@
 console.log("Compassion = Happiness");
+const pokedex = document.getElementById("pokedex");
 
-const pokemonContainer = document.getElementById(`pokemonContainer`);
-const pokemonAmount = 151;
-
-// function to call pokemon
-const fetchPokemon = async () => {
-  for (let i = 1; i <= pokemonAmount; i++) {
-    await getPokemon(i);
+const fetchPokemon = () => {
+  const promises = [];
+  for (let i = 1; i <= 151; i++) {
+    const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
+    promises.push(fetch(url).then((res) => res.json()));
   }
+  Promise.all(promises).then((results) => {
+    const pokemon = results.map((result) => ({
+      name: result.name,
+      image: result.sprites["front_default"],
+      type: result.types.map((type) => type.type.name).join(", "),
+      id: result.id,
+    }));
+    displayPokemon(pokemon);
+  });
 };
 
-const getPokemon = async (id) => {
-  const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
-  const res = await fetch(url);
-  const pokemon = await res.json();
-  makePokemonCard(pokemon);
+const displayPokemon = (pokemon) => {
+  console.log(pokemon);
+  const pokemonHTMLString = pokemon
+    .map(
+      (pokeman) => `
+        <li class="card">
+            <img class="card-image" src="${pokeman.image}"/>
+            <h2 class="card-title">${pokeman.id}. ${pokeman.name}</h2>
+            <p class="card-subtitle">Type: ${pokeman.type}</p>
+        </li>
+    `
+    )
+    .join("");
+  pokedex.innerHTML = pokemonHTMLString;
 };
 
-// shows return data
 fetchPokemon();
-
-function makePokemonCard(pokemon) {
-  const pokemonElement = document.createElement(`div`);
-  pokemonElement.classList.add(`pokemon`);
-
-  const name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1);
-
-  const pokemonInnerHTML = `
-  <div class="image-container"></div>
-  
-    pokemonElement.innerHTML = pokemonInnerHTML;
-  pokemonContainer.appendChild(pokemonElement);
-}
